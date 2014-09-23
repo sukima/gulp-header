0d47539ce318f5f23d4a1bb4be74e471b50cc8e6
Devin Weaver <suki@tritarget.org>
2014-09-23 14:04:12 -0400

    Add source map adjustment support

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 var through = require('through2');
 var gutil = require('gulp-util');
 var extend = require('lodash.assign');
+var SourceMapGenerator = require('source-map').SourceMapGenerator;
+var SourceMapConsumer = require('source-map').SourceMapConsumer;
 
 var headerPlugin = function(headerText, data) {
     headerText = headerText || '';
@@ -24,6 +26,35 @@ var headerPlugin = function(headerText, data) {
             stream.write(new Buffer(template));
             stream.on('error', this.emit.bind(this, 'error'));
             file.contents = file.contents.pipe(stream);
+        }
+
+        if (file.sourceMap) {
+            var lines = template.split(/\r\n|\r|\n/).length;
+            var smc = new SourceMapConsumer(file.sourceMap);
+            var generator = SourceMapGenerator.fromSourceMap(smc);
+
+            smc.eachMapping(function(mapping) {
+                var adjuster = (lines === 1) ? 'column' : 'line';
+                var offset = lines > 1 ? lines : template.length;
+                var generated = {
+                    line: mapping.generatedLine,
+                    column: mapping.generatedColumn
+                };
+
+                generated[adjuster] += offset;
+
+                generator.addMapping({
+                    original: {
+                        line: mapping.originalLine,
+                        column: mapping.originalColumn
+                    },
+                    generated: generated,
+                    source: mapping.source,
+                    name: mapping.name
+                });
+            });
+
+            file.sourceMap = JSON.parse(generator.toString());
         }
 
         // make sure the file goes through the next gulp plugin
